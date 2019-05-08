361936912caca503cfd895c69f95378e4808074d
Mickey Boxell <mickey.boxell@oracle.com>
2019-05-08 09:58:51 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
+/*
+** Copyright © 2019 Oracle and/or its affiliates. All rights reserved.
+** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+*/
 export const regions = ['ca-toronto-1', 'eu-frankfurt-1', 'uk-london-1', 'us-ashburn-1', 'us-phoenix-1']
 export const namespaces = ['oci_computeagent', 'oci_blockstore', 'oci_lbaas', 'oci_telemetry']
 export const aggregations = ['count()', 'max()', 'mean()', 'min()', 'rate()', 'sum()', 'percentile(.90)', 'percentile(.95)', 'percentile(.99)']
