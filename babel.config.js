8449557d466005fc788d1fe7cdea29d520ec1261
Mickey Boxell <mickey.boxell@oracle.com>
2019-05-15 12:14:06 -0700

    Update babel.config.js
    Removed erroneous module

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,6 @@
 ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 */
 module.exports = {
-    "extends": "standard",
-    "rules": {
-      "import/no-webpack-loader-syntax": 0,
-    },
-    "env": {
-      "jest": true
-    }
-};
-module.exports = {
   presets: [
     [
       '@babel/preset-env',
