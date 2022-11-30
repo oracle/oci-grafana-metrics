36f20d271ddf9ed56631b67b6bc0980a2735272d
Mattia Moretti <mattia.moretti@oracle.com>
2022-11-30 08:31:34 +0100

    ready to merge

--- src/query_ctrl.js
+++ src/query_ctrl.js
@@ -1,5 +1,5 @@
 /*
-** Copyright © 2019 Oracle and/or its affiliates. All rights reserved.
+** Copyright © 2022 Oracle and/or its affiliates. All rights reserved.
 ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 */
 import { QueryCtrl } from 'app/plugins/sdk'
