130eb26968d1aeb69bce6795f7328b50386e0221
Mattia Moretti <mattia.moretti@oracle.com>
2023-05-19 13:11:03 +0200

    Mig2 (#200)
    * last steps
    
    * no error
    
    * it compiles
    
    * mig phase 2 skeleton

---
+++ src/query_model.ts
@@ -0,0 +1,100 @@
+import { OCIQuery, QueryPlaceholder, AggregationOptions, IntervalOptions } from './types';
+import { ScopedVars } from '@grafana/data';
+import { TemplateSrv } from '@grafana/runtime';
+
+export default class QueryModel {
+  target: OCIQuery;
+  templateSrv: any;
+  scopedVars: any;
+  refId?: string;
+
+  constructor(incomingQuery: OCIQuery, templateSrv?: TemplateSrv, scopedVars?: ScopedVars) {
+    this.target = incomingQuery;
+    this.templateSrv = templateSrv;
+    this.scopedVars = scopedVars;
+
+    this.target.tenancyOCID = incomingQuery.tenancyOCID || QueryPlaceholder.Tenancy;
+    // this.target.compartmentOCID = incomingQuery.compartmentOCID || QueryPlaceholder.Compartment;
+    this.target.compartmentOCID = incomingQuery.compartmentOCID || '';
+    this.target.region = incomingQuery.region || QueryPlaceholder.Region;
+    this.target.namespace = incomingQuery.namespace || QueryPlaceholder.Namespace;
+    this.target.metric = incomingQuery.metric || QueryPlaceholder.Metric;
+    this.target.statistic = incomingQuery.statistic || QueryPlaceholder.Aggregation;
+    this.target.interval = incomingQuery.interval || QueryPlaceholder.Interval;
+    this.target.resourceGroup = incomingQuery.resourceGroup || QueryPlaceholder.ResourceGroup;
+    this.target.dimensionValues = incomingQuery.dimensionValues || [];
+    this.target.tagsValues = incomingQuery.tagsValues || [];
+    this.target.groupBy = incomingQuery.groupBy || QueryPlaceholder.GroupBy;
+
+    this.target.hide = incomingQuery.hide ?? false;
+
+    if (this.target.resourceGroup === QueryPlaceholder.ResourceGroup) {
+      this.target.resourceGroup = '';
+    }
+
+    // handle pre query gui panels gracefully, so by default we will have raw editor
+    this.target.rawQuery = incomingQuery.rawQuery ?? true;
+
+    if (this.target.rawQuery) {
+      this.target.queryText =
+        incomingQuery.queryText || 'metric[interval]{dimensionname="dimensionvalue"}.groupingfunction.statistic';
+    } else {
+      this.target.queryText = incomingQuery.queryText || this.buildQuery();
+    }
+  }
+
+  isQueryReady() {
+    // check if the query is ready to be built
+    if (
+      this.target.tenancyOCID === QueryPlaceholder.Tenancy ||
+      this.target.region === QueryPlaceholder.Region ||
+      this.target.namespace === QueryPlaceholder.Namespace ||
+      this.target.metric === QueryPlaceholder.Metric
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  buildQuery() {
+    let queryText = this.target.metric;
+
+    // for default interval
+    if (this.target.interval === QueryPlaceholder.Interval) {
+      this.target.interval = IntervalOptions[0].value;
+    }
+    queryText += this.target.interval;
+
+    // for dimensions
+    let dimensionParams = '{';
+    let noOfDimensions = this.target.dimensionValues?.length ?? 0;
+    if (noOfDimensions !== 0) {
+      this.target.dimensionValues?.forEach((dv) => {
+        dimensionParams += dv;
+        noOfDimensions--;
+
+        if (noOfDimensions !== 0) {
+          dimensionParams += ',';
+        }
+      });
+      dimensionParams += '}';
+
+      queryText += dimensionParams;
+    }
+
+    // for groupBy option
+    if (this.target.groupBy !== QueryPlaceholder.GroupBy) {
+      queryText += '.groupBy(' + this.target.groupBy + ')';
+    }
+
+    // for default statistics
+    if (this.target.statistic === QueryPlaceholder.Aggregation) {
+      this.target.statistic = AggregationOptions[0].value;
+    }
+
+    queryText += '.' + this.target.statistic;
+
+    return queryText;
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,15 @@
 import { DataSourceJsonData } from '@grafana/data';
 import { DataQuery } from '@grafana/schema';
 
-export interface OCIQuery extends DataQuery {
-  multiplier: number;
-  queryType: string;
-  region: string;
-  compartment: string;
-  tenancy: string;
-  namespace: string
-  resourcegroup: string;  
-}
+// export interface OCIQuery extends DataQuery {
+//   multiplier: number;
+//   queryType: string;
+//   region: string;
+//   compartment: string;
+//   tenancy: string;
+//   namespace: string
+//   resourcegroup: string;  
+// }
 
 export interface MyDataSourceOptions extends DataSourceJsonData {}
 
@@ -163,3 +163,117 @@ export interface MultiFilter extends CommonFilterProps {
 export type Filter = NullFilter | BooleanFilter | NumberFilter | DateFilter | StringFilter | MultiFilter;
 
 //#endregion
+
+export enum DefaultOCIOptions {
+  ConfigPath = '~/.oci/config',
+  MultiTenanciesFile = '~/.oci/tenancies',
+  ConfigProfile = 'DEFAULT',
+}
+
+export enum OCIResourceCall {
+  Tenancies = 'tenancies',
+  Compartments = 'compartments',
+  Regions = 'regions',
+  Namespaces = 'namespaces',
+  ResourceGroups = 'resourcegroups',
+  Dimensions = 'dimensions',
+  Tags = 'tags',
+}
+
+export enum QueryPlaceholder {
+  Tenancy = 'select tenancy',
+  Compartment = 'select compartment (optional)',
+  Region = 'select region',
+  Namespace = 'select namespace',
+  Metric = 'select metric',
+  Aggregation = 'select aggregation',
+  Interval = 'select interval',
+  Dimensions = 'select dimensions (optional)',
+  ResourceGroup = 'select resourcegroup (optional)',
+  Tags = 'select resource tags (optional)',
+  GroupBy = 'select option (optional)',
+}
+
+export interface DimensionPart {
+  type: string;
+  params: Array<string | number>;
+  name?: string;
+}
+
+export type UnitOptions = 'minute' | 'hour';
+
+export const IntervalOptions = [
+  { label: '1 minute', value: '[1m]', description: 'Maximum time range supported: 7 days' },
+  { label: '5 minutes', value: '[5m]', description: 'Maximum time range supported: 30 days' },
+  { label: '1 hour', value: '[1h]', description: 'Maximum time range supported: 90 days' },
+  { label: '1 day', value: '[1d]', description: 'Maximum time range supported: 90 days' },
+];
+
+export const AggregationOptions = [
+  { label: 'average', value: 'avg()' },
+  { label: 'count', value: 'count()' },
+  { label: 'per-interval change', value: 'increment()' },
+  { label: 'maximum', value: 'max()' },
+  { label: 'minimum', value: 'min()' },
+  { label: 'per-interval average rate of change', value: 'rate()' },
+  { label: 'sum', value: 'sum()' },
+  { label: 'P90', value: 'percentile(.90)' },
+  { label: 'P95', value: 'percentile(.95)' },
+  { label: 'P99', value: 'percentile(.99)' },
+  { label: 'P99.9', value: 'percentile(.999)' },
+];
+
+export const GroupOptions = [
+  { label: 'group by', value: 'groupBy()' },
+  { label: 'grouping', value: 'grouping()' },
+];
+
+export interface OCIQuery extends DataQuery {
+  queryText?: string;
+  rawQuery: boolean;
+  //hide: boolean;
+  tenancyName: string;
+  tenancyOCID: string;
+  compartments?: any;
+  compartmentName?: string;
+  compartmentOCID?: string;
+  regions?: any;
+  region?: string;
+  namespace?: string;
+  merticNames?: string[];
+  merticNamesFromNS?: string[];
+  metric?: string;
+  interval: string;
+  intervalLabel?: string;
+  statistic: string;
+  statisticLabel?: string;
+  resourceGroup?: string;
+  dimensionValues?: string[];
+  tagsValues?: string[];
+  groupBy?: string;
+}
+
+export const defaultQuery: Partial<OCIQuery> = {};
+
+/**
+ * These are options configured for each DataSource instance
+ */
+export interface OCIDataSourceOptions extends DataSourceJsonData {
+  tenancyName: string; // name of the base tenancy
+  authProvider: string; // oci-cli, oci-instance
+  multiTenancyMode?: string; // multi-profile, cross-tenancy-policy
+  multiTenancyChoice?: string; // yes, no
+  multiTenancyFile?: string; // Default is ~/.oci/tenancies, if enabled
+  configPath?: string; // Config file path. Default is ~/.oci/config
+  configProfile?: string; // Config profile name, as specified in ~/.oci/config. Default is DEFAULT
+  enableCMDB?: boolean; // Choice to enable oracel cmdb datasource mapping
+  enableCMDBUploadFile?: boolean; // Choice to enable user to upload customer mapping file
+  cmdbFileContent: string; // CMDB file content as json string
+}
+
+/**
+ * Value that is used in the backend, but never sent over HTTP to the frontend
+ */
+export interface OCISecureJsonData {
+  // nothing for now
+}
