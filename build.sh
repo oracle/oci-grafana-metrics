c35efd984acb1261117383f4df80cdb016fce554
John Ciprian <john.ciprian@oracle.com>
2026-07-08 15:59:18 -0400

    Fix marketplace backend plugin packaging

--- build.sh
+++ build.sh
@@ -1,53 +1,50 @@
 #!/bin/bash
+set -euo pipefail
 
-#Do grunt work
-# nvm install 12.20
-# nvm use 12.20
+PLUGIN_ID="oci-metrics-datasource"
+VERSION=$(node -p "require('./package.json').version")
+ARCHIVE="${PLUGIN_ID}-${VERSION}.zip"
 
 if [[ ! -d ./node_modules ]]; then
   echo "dependencies not installed try running: yarn"
   exit 1
 fi
-rm -rf ./oci-metrics-datasource
-rm ./oci-metrics-datasource.zip 
-rm ./plugin.tar
-# yarn create @grafana/plugin
-yarn run build
-if [ $? -ne 0 ]; then
-    echo "yarn returned error"
-    exit 1
-fi
 
-mage --debug -v
+rm -rf ./dist "./${PLUGIN_ID}" "./${ARCHIVE}"
+
+yarn run build
+mage -v
 
 cp LICENSE.txt ./dist/LICENSE
 
-if [ -z $1 ]; then
-  echo "sign argument not specified, continuing without sign the plugin"
-else
-  if [ $1 = "sign" ]; then
-    npx @grafana/sign-plugin
-  else
-    echo "Usage: ./build.sh <sign>"
-  fi  
+for binary in \
+  oci-metrics-plugin_darwin_amd64 \
+  oci-metrics-plugin_darwin_arm64 \
+  oci-metrics-plugin_linux_amd64 \
+  oci-metrics-plugin_linux_arm \
+  oci-metrics-plugin_linux_arm64 \
+  oci-metrics-plugin_windows_amd64.exe; do
+  if [[ ! -x "./dist/${binary}" ]]; then
+    echo "missing executable backend binary: ${binary}"
+    exit 1
+  fi
+done
+
+if [[ ! -f ./dist/go_plugin_build_manifest ]]; then
+  echo "missing Go plugin build manifest"
+  exit 1
 fi
 
-mv ./dist ./oci-metrics-datasource
-tar cvf plugin.tar ./oci-metrics-datasource
-zip -r oci-metrics-datasource ./oci-metrics-datasource
+if [[ -z "${1:-}" ]]; then
+  echo "sign argument not specified, continuing without sign the plugin"
+elif [[ "$1" == "sign" ]]; then
+  yarn sign
+else
+  echo "Usage: ./build.sh [sign]"
+  exit 1
+fi
 
-# Instructions for signing
-# Please make sure
-# nvm install 12.20
+cp -R ./dist "./${PLUGIN_ID}"
+zip -qr "./${ARCHIVE}" "./${PLUGIN_ID}"
 
-# nvm use 12.20
-
-# yarn
-# For grafana publishing
-# yarn install --pure-lockfile && yarn build
-#
-# Please make sure if you have the api keys installed in bash profile in name,  GRAFANA_API_KEY
-# Note : Please make sure that you are running the commands in a non-proxy env and without vpn, else grafana signing might fail"
-# yarn  global add @grafana/toolkit
-# grafana-toolkit plugin:sign
-
+echo "created ${ARCHIVE}"
